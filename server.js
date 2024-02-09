fac2305293685debf42d080debcf16af17458a93
noufanelachola <noufansadiqelachola@gmail.com>
2024-02-09 19:56:22 +0530

    bcrypt added in register

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const knex = require("knex");
+const bcrypt = require("bcrypt-nodejs");
 
 const register = require("./routes/register");
 
@@ -24,7 +25,7 @@ app.get("/",(req,res) => {
         .catch(err => res.status(400).json("error"));
 })
 
-app.post("/school/register",(req,res) => {register.handleRegister(req,res,db)});
+app.post("/school/register",(req,res) => {register.handleRegister(req,res,db,bcrypt)});
 
 app.listen(3000,() => {
     console.log(`App is running on port 3000`);
