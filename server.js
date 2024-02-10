a8fee6acd2527bc6e2ed39a7fe535f0e7a30546b
noufanelachola <noufansadiqelachola@gmail.com>
2024-02-10 18:28:17 +0530

    funtion for adding students created

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const app = express();
 const knex = require("knex");
 const bcrypt = require("bcrypt-nodejs");
 
-const register = require("./routes/register");
-const signin = require("./routes/signin");
+const register = require("./controllers/school/register");
+const signin = require("./controllers/school/signin");
+const studentAdd = require("./controllers/student/studentAdd");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -26,8 +27,13 @@ app.get("/",(req,res) => {
         .catch(err => res.status(400).json("error"));
 })
 
+// route to register schools by admin
 app.post("/school/register",(req,res) => {register.handleRegister(req,res,db,bcrypt)});
+// route to signin by users
 app.post("/school/signin",(req,res) => {signin.handleSignIn(req,res,db,bcrypt)});
+
+// route to add students
+app.post("/student/add",(req,res) => {studentAdd.handleStudentAdd(req,res,db)});
 
 app.listen(3000,() => {
     console.log(`App is running on port 3000`);
