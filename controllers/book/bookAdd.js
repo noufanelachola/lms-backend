7ae07d015400cc956c698eb688150143619ff39e
noufanelachola <noufansadiqelachola@gmail.com>
2024-06-19 16:01:03 +0530

    created fn to handel book add

--- controllers/book/bookAdd.js
+++ controllers/book/bookAdd.js
@@ -1,5 +1,24 @@
 const handleBookAdd = (req,res,db) => {
-    res.json("helloo")
+    const {schoolId,bookName,bookAuthor,totalCopies} = req.body;
+
+    db.transaction(trx => {
+        trx.insert({
+            schoolid : schoolId,
+            bookname : bookName,
+            bookauthor : bookAuthor,
+            totalcopies : totalCopies,
+            availablecopies : totalCopies
+        }).into("books")
+        .returning("*")
+        .then(book => {
+            res.json(book[0]);
+            trx.commit();
+        }).catch(error => {
+            trx.rollback();
+            res.status(400).json("Couldn't add book");
+            console.log(error);
+        });
+    });
 }
 
 module.exports = {
