250b11176385ab5708b760a4aaff7a2701bc8e9f
noufanelachola <noufansadiqelachola@gmail.com>
2024-06-29 09:20:45 +0530

    small featues adds

--- controllers/school/signin.js
+++ controllers/school/signin.js
@@ -1,20 +1,28 @@
 const handleSignIn = (req,res,db,bcrypt) => {
     const {username,password} = req.body;
-    db.select("*")
-        .from("school")
-        .where("username",username)
-        .then(school => {
-            const access = bcrypt.compareSync(password,school[0].password);
-            if(access){
-                res.json(school[0]);
-            } else {
-                res.status(400).json("Wrong Credentials!!");
-            }
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json("Internal server error");
+    db.select(
+        "id",
+        "username",
+        "password",
+        "school_name",
+        db.raw("TO_CHAR(start_date, 'DD-MM-YYYY') as start_date"),
+        db.raw("TO_CHAR(end_date, 'DD-MM-YYYY') as end_date"),
+        "is_active"
+    )
+    .from("school")
+    .where("username",username)
+    .then(school => {
+        const access = bcrypt.compareSync(password,school[0].password);
+        if(access){
+            res.json(school[0]);
+        } else {
+            res.status(400).json("Wrong Credentials!!");
         }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json("Internal server error");
+    }
     );
 }
 
