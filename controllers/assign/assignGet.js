588cc0f1824829a8e5b7807db58d6e5d22cda73b
noufanelachola <noufansadiqelachola@gmail.com>
2024-06-30 21:55:33 +0530

    profile for students

--- controllers/assign/assignGet.js
+++ controllers/assign/assignGet.js
@@ -1,32 +1,35 @@
 const handleAssignGet = (req,res,db) => {
-    const {schoolId} = req.query;
+    const {schoolId,studentId,status} = req.query;
 
-    // db.select("*")
-    // .from("transactions")
-    // .where({schoolid : schoolId, status : "pending"})
-    // .then(transaction => res.json(transaction))
-    // .catch(error => {
-    //     res.status(400).json("Error while getting assign students");
-    //     console.log(error);
-    // });
+    let query = db.from("transactions as t")
+        .join("students as s","t.studentid","s.studentid")
+        .join("books as b","t.bookid","b.bookid")
+        .select(
+            "t.transactionid",
+            "s.studentid",
+            "b.bookid",
+            db.raw("TO_CHAR(t.issue_date, 'DD-MM-YYYY') as issue_date"),
+            db.raw("TO_CHAR(t.due_date, 'DD-MM-YYYY') as due_date"),
+            "t.status",
+            "s.studentname",
+            "s.studentclass",
+            "s.admissionnumber",
+            "b.bookname",
+            "b.bookauthor"
+        ).where({"t.schoolid" : schoolId});
 
-    db.from("transactions as t")
-    .join("students as s","t.studentid","s.studentid")
-    .join("books as b","t.bookid","b.bookid")
-    .select(
-        "t.transactionid",
-        "s.studentid",
-        "b.bookid",
-        db.raw("TO_CHAR(t.issue_date, 'DD-MM-YYYY') as issue_date"),
-        db.raw("TO_CHAR(t.due_date, 'DD-MM-YYYY') as due_date"),
-        "t.status",
-        "s.studentname",
-        "s.studentclass",
-        "s.admissionnumber",
-        "b.bookname",
-        "b.bookauthor"
-    ).where({"t.schoolid" : schoolId,status:"pending"})
-    .then(transaction => res.json(transaction))
+    if (studentId){
+        query = query.andWhere("s.studentid",studentId);
+    }
+    
+    if (status === "pending"){
+        query = query.andWhere("status","pending");
+    }
+    else if (status === "submitted"){
+        query = query.andWhere("status","submitted");
+    }
+
+    query.then(transaction => res.json(transaction))
     .catch(error => {
         res.status(400).json("couldnt get the assign students");
         console.log(error);
